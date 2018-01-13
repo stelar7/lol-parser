1c81b7428b90351852b85c2ac6ec56d6c8bbff74
stelar7 <dudedbz@gmail.com>
2018-01-13 04:49:51 +0100

    Add decompression test to easily uncompress all compressed files

---
+++ src/test/java/util/TestUncompress.java
@@ -0,0 +1,28 @@
+package util;
+
+import no.stelar7.cdragon.util.*;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class TestUncompress
+{
+    @Test
+    public void testAll() throws IOException
+    {
+        Files.walkFileTree(Paths.get(System.getProperty("user.home"), "Downloads"), new SimpleFileVisitor<Path>()
+        {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            {
+                if (file.toString().endsWith(".compressed"))
+                {
+                    CompressionHandler.uncompressDEFLATE(file, Paths.get(System.getProperty("user.home"), "Downloads\\decompressed\\" + UtilHandler.pathToFilename(file)));
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
