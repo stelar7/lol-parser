8ae65c7c7b6dba604443a3e5513ea9eb57553a04
stelar7 <dudedbz@gmail.com>
2018-07-07 13:27:26 +0200

    Add wad extractor

---
+++ src/main/java/no/stelar7/cdragon/viewer/WadExtracter.java
@@ -0,0 +1,30 @@
+package no.stelar7.cdragon.viewer;
+
+import no.stelar7.cdragon.types.wad.WADParser;
+import no.stelar7.cdragon.types.wad.data.WADFile;
+
+import java.nio.file.*;
+
+public class WadExtracter
+{
+    public static void main(String[] args)
+    {
+        WADParser parser = new WADParser();
+        
+        if (args.length != 2)
+        {
+            System.out.println("Needs 2 parameters! (wad file, and output folder)");
+        }
+        
+        Path output = Paths.get(args[1]);
+        if (!Files.isDirectory(output))
+        {
+            System.out.println("Output is not a folder");
+        }
+        
+        Path    path = Paths.get(args[0]);
+        WADFile img  = parser.parse(path);
+        
+        img.extractFiles("", "", path);
+    }
+}
