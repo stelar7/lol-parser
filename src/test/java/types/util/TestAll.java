fc6b958107f290363be2a229b488ed373da41ffc
stelar7 <dudedbz@gmail.com>
2018-12-17 20:29:01 +0100

    Remove references to SecretFile.java

--- src/test/java/types/util/TestAll.java
+++ src/test/java/types/util/TestAll.java
@@ -32,7 +32,6 @@ public class TestAll
         deleteUnknownFolder();
         downloadWAD();
         extractImages();
-        //uploadToFTP();
     }
     
     private void deleteUnknownFolder() throws IOException
@@ -1695,60 +1694,4 @@ public class TestAll
         }
     }
     
-    public void uploadToFTP() throws IOException
-    {
-        List<String> prefix = new ArrayList<>(Arrays.asList("www", "cdragon", "latest"));
-        
-        FTPClient client = new FTPClient();
-        client.connect("ftp.domeneshop.no");
-        client.login(SecretFile.USERNAME, SecretFile.PASSWORD);
-        client.setFileType(FTP.BINARY_FILE_TYPE);
-        client.changeWorkingDirectory(prefix.stream().reduce("", (o, n) -> o + "/" + n));
-        
-        Path files = UtilHandler.DOWNLOADS_FOLDER.resolve("rcp-be-lol-game-data\\pretty\\");
-        Files.walkFileTree(files, new SimpleFileVisitor<>()
-        {
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException
-            {
-                if (UtilHandler.pathToFolderName(dir).equalsIgnoreCase("pretty"))
-                {
-                    return FileVisitResult.CONTINUE;
-                }
-                
-                String folder = UtilHandler.pathToFolderName(dir);
-                
-                prefix.add(folder);
-                client.makeDirectory(folder);
-                client.changeWorkingDirectory(prefix.stream().reduce("", (o, n) -> o + "/" + n));
-                
-                System.out.println("Uploading to " + client.printWorkingDirectory());
-                
-                return FileVisitResult.CONTINUE;
-            }
-            
-            @Override
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException
-            {
-                prefix.remove(UtilHandler.pathToFolderName(dir));
-                client.changeWorkingDirectory(prefix.stream().reduce("", (o, n) -> o + "/" + n));
-                return FileVisitResult.CONTINUE;
-            }
-            
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
-            {
-                try (FileInputStream is = new FileInputStream(file.toFile()))
-                {
-                    client.storeFile(file.getFileName().toString(), is);
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        });
-        
-        
-        client.logout();
-        client.disconnect();
-        
-    }
 }
