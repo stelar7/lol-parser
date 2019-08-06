da20022f5e178d194a1b474a38bd3df7f48dca26
stelar7 <dudedbz@gmail.com>
2019-08-07 00:07:09 +0200

    delete voice line search as its covered by the default search

--- src/main/java/no/stelar7/cdragon/util/hashguessing/GameHashGuesser.java
+++ src/main/java/no/stelar7/cdragon/util/hashguessing/GameHashGuesser.java
@@ -1,11 +1,9 @@
 package no.stelar7.cdragon.util.hashguessing;
 
-import com.google.gson.*;
-import com.google.gson.stream.*;
+import com.google.gson.JsonElement;
 import no.stelar7.cdragon.util.handlers.UtilHandler;
 
-import java.io.*;
-import java.nio.file.*;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.regex.*;
 
@@ -15,36 +13,6 @@ public class GameHashGuesser extends HashGuesser
     public GameHashGuesser(Set<String> hashes)
     {
         super(HashGuesser.hashFileGAME, hashes);
-    }
-    
-    public void guessVoiceLines(Path pbe)
-    {
-        System.out.println("Guessing new voice lines");
-        List<Path> readMe = UtilHandler.getFilesMatchingPredicate(pbe, UtilHandler.IS_JSON_PREDICATE);
-        
-        readMe.stream()
-              .map(UtilHandler::readAsString)
-              .filter(s -> s.contains("bankUnits"))
-              .map(s -> UtilHandler.getJsonParser().parse(s))
-              .map(JsonElement::getAsJsonObject)
-              .forEach(e -> {
-                  String     data   = "{" + e.toString().substring(e.toString().indexOf("bankUnits") - 1);
-                  JsonReader reader = new JsonReader(new StringReader(data));
-                  reader.setLenient(true);
-            
-                  JsonArray bankUnits = UtilHandler.getJsonParser().parse(reader).getAsJsonObject().getAsJsonArray("bankUnits");
-                  for (JsonElement unit : bankUnits)
-                  {
-                      String    child = unit.getAsJsonObject().keySet().toArray(new String[0])[0];
-                      JsonArray paths = unit.getAsJsonObject().getAsJsonObject(child).getAsJsonArray("bankPath");
-                
-                      for (JsonElement path : paths)
-                      {
-                          String real = path.getAsString().toLowerCase();
-                          this.check(real);
-                      }
-                  }
-              });
     }
     
     public void guessBinByLinkedFiles(Path pbe)
